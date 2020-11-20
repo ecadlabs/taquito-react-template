fff419c35981a4bba1f1c74751ea93ef635c409b
Sean Magin <sean@arrowburn.co.nz>
2020-11-21 09:57:44 +1300

    update structure and types on UpdateContract componennt

--- src/components/UpdateContract.tsx
+++ src/components/UpdateContract.tsx
@@ -2,7 +2,7 @@ import React, { useState, Dispatch, SetStateAction } from "react";
 import { TezosToolkit, WalletContract } from "@taquito/taquito";
 
 interface UpdateContractProps {
-  contract: WalletContract | null;
+  contract: WalletContract | any;
   setUserBalance: Dispatch<SetStateAction<any>>;
   Tezos: TezosToolkit;
   userAddress: string;
@@ -13,66 +13,63 @@ const UpdateContract = ({ contract, setUserBalance, Tezos, userAddress, setStora
   const [loadingIncrement, setLoadingIncrement] = useState<boolean>(false);
   const [loadingDecrement, setLoadingDecrement] = useState<boolean>(false);
 
-  if (contract && userAddress) {
-    const increment = async (): Promise<void> => {
-      setLoadingIncrement(true);
-      try {
-        const op = await contract.methods.increment(1).send();
-        await op.confirmation();
-        const newStorage: any = await contract.storage();
-        if (newStorage) setStorage(newStorage.toNumber());
-        setUserBalance(await Tezos.tz.getBalance(userAddress));
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoadingIncrement(false);
-      }
-    };
+  const increment = async (): Promise<void> => {
+    setLoadingIncrement(true);
+    try {
+      const op = await contract.methods.increment(1).send();
+      await op.confirmation();
+      const newStorage: any = await contract.storage();
+      if (newStorage) setStorage(newStorage.toNumber());
+      setUserBalance(await Tezos.tz.getBalance(userAddress));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingIncrement(false);
+    }
+  };
 
-    const decrement = async (): Promise<void> => {
-      setLoadingDecrement(true);
-      try {
-        const op = await contract.methods.decrement(1).send();
-        await op.confirmation();
-        const newStorage: any = await contract.storage();
-        if (newStorage) setStorage(newStorage.toNumber());
-        setUserBalance(await Tezos.tz.getBalance(userAddress));
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoadingDecrement(false);
-      }
-    };
+  const decrement = async (): Promise<void> => {
+    setLoadingDecrement(true);
+    try {
+      const op = await contract.methods.decrement(1).send();
+      await op.confirmation();
+      const newStorage: any = await contract.storage();
+      if (newStorage) setStorage(newStorage.toNumber());
+      setUserBalance(await Tezos.tz.getBalance(userAddress));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingDecrement(false);
+    }
+  };
 
-    return (
-      <div className="buttons">
-        <button className="button" disabled={loadingIncrement} onClick={increment}>
-          {loadingIncrement ? (
-            <span>
-              <i className="fas fa-spinner fa-spin"></i>&nbsp; Please wait
-            </span>
-          ) : (
-            <span>
-              <i className="fas fa-plus"></i>&nbsp; Increment by 1
-            </span>
-          )}
-        </button>
-        <button className="button" onClick={decrement}>
-          {loadingDecrement ? (
-            <span>
-              <i className="fas fa-spinner fa-spin"></i>&nbsp; Please wait
-            </span>
-          ) : (
-            <span>
-              <i className="fas fa-minus"></i>&nbsp; Decrement by 1
-            </span>
-          )}
-        </button>
-      </div>
-    );
-  } else {
-    return <div>&nbsp;</div>;
-  }
+  if (!contract && !userAddress) return <div>&nbsp;</div>;
+  return (
+    <div className="buttons">
+      <button className="button" disabled={loadingIncrement} onClick={increment}>
+        {loadingIncrement ? (
+          <span>
+            <i className="fas fa-spinner fa-spin"></i>&nbsp; Please wait
+          </span>
+        ) : (
+          <span>
+            <i className="fas fa-plus"></i>&nbsp; Increment by 1
+          </span>
+        )}
+      </button>
+      <button className="button" onClick={decrement}>
+        {loadingDecrement ? (
+          <span>
+            <i className="fas fa-spinner fa-spin"></i>&nbsp; Please wait
+          </span>
+        ) : (
+          <span>
+            <i className="fas fa-minus"></i>&nbsp; Decrement by 1
+          </span>
+        )}
+      </button>
+    </div>
+  );
 };
 
 export default UpdateContract;
