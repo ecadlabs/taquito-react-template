917c0607cb4527d47e22c740fd2136f6ee265ed3
Maxwell Ward <max.jp.ward@gmail.com>
2025-09-03 12:45:38 -0700

    remove unused import

--- src/components/UpdateContract.tsx
+++ src/components/UpdateContract.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Dispatch, SetStateAction } from "react";
+import { useState, Dispatch, SetStateAction } from "react";
 import { TezosToolkit, WalletContract } from "@taquito/taquito";
 
 interface UpdateContractProps {
